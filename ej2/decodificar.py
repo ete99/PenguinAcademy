0f403f5a2db0ad35e194105849c21f79c33887c7
ete99 <stefanvallet99@gmail.com>
2020-01-31 20:17:02 -0300

    ej2

--- ej2/decodificar.py
+++ ej2/decodificar.py
@@ -1,14 +1,14 @@
 def desplazar(pal, desp):
-    return pal[desp:]+pal[:desp]
+    return pal[desp:]+pal[:desp]  # decodifica
 
 arr = ['oscreem', 'ueq', 'la', 'óncodificaci', 'ñaense', 'eshabilidad', 'eld', 'losig', '21', 'osusam', 'la', 'óncodificaci', 'moco', 'nau', 'taherramien', 'rapa', 'armostr', 'moco']
 
 new_arr=[]
 for i in arr:
-    new_arr.append(desplazar(i, 2))
-print(new_arr)
+    new_arr.append(desplazar(i, 2))  # desplaza y adhiere al nuevo array
+print(new_arr)  # imprime el array
 
 resultado=''
 for i in new_arr:
     resultado = resultado + ' ' + i
-#print(resultado)  # para dejar en una oracion
+#print(resultado)  # imprime el resultado en una oracion en una oracion
